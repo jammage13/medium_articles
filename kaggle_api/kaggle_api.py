c08c5ac22bff6d028642d622ee99ac7e7a1a60b4
jammage13 <jamie.southall90@gmail.com>
2022-11-02 21:33:41 +0000

    completed kaggle_api.py

--- kaggle_api/kaggle_api.py
+++ kaggle_api/kaggle_api.py
@@ -7,18 +7,21 @@ Created on Tue Nov  1 19:47:13 2022
 """
 
 import kaggle
+import zipfile
+import os
 
 api = kaggle.api
 
-api.dataset_list(search='titanic')
-api.dataset_list()
-
 api.competitions_list()
-api.competition_download_files('titanic')
+
+api.competitions_list(search='titanic')
 
 api.competitions_data_list_files('titanic')
 
-api.competitions_data_list_files_with_http_info('titanic')
+api.competition_download_files('titanic')
 
-api.dataset_download_file('titanic/data//itanic/train.csv', 'train.csv')
-api.dataset_download_files('titanic/data')
+with zipfile.ZipFile('titanic.zip', 'r') as zip_ref:
+    zip_ref.extractall('C:/Users/Jam/Documents/Python Scripts/Medium/medium_articles/kaggle_api')
+    
+os.listdir()
+
